22278191f9b224509b5dd549b46e0e7681dd7a3d
Peter <Peni419@student.sdu.dk>
2022-05-16 14:50:55 +0200

    Initial Commit

---
+++ ProductBackendCrawler/src/main/java/com/serchhub/services/ProductBackendCrawler/Controller/WebcrawlerController.java
@@ -0,0 +1,8 @@
+package com.serchhub.services.ProductBackendCrawler.Controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebcrawlerController {
+
+}
---
+++ ProductBackendCrawler/src/test/java/com/serchhub/services/ProductBackendCrawler/ProductBackendCrawlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.serchhub.services.ProductBackendCrawler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductBackendCrawlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
